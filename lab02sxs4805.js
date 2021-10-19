af3b5be4cc8ceb88e1d2847a06023222886e34ab
Shubhayu Shrestha <shresthashubhayu@gmail.com>
2021-10-19 11:51:12 -0500

    Add files via upload

--- lab02sxs4805.js
+++ lab02sxs4805.js
@@ -2,13 +2,14 @@
 // STUDENT ID: 1001724804
 // DUE DATE: October 20th, 2021
 
-// Question 1
-// Creating an array called inputtable which holds numbers 1 through 10
+// Sources Used: JavaScript Documentation 
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript
 
+// Question 1: Create an array called inputtable that holds numbers 1 through 10
+// Creating an array called inputtable which holds numbers 1 through 10
 const inputtable = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-// Question 2
-// Use inputtable to create a set of multiplesof 5 between 1 and 51.
+// Question 2: Use inputtable to create a set of multiplesof 5 between 1 and 51.
 // Create another array called fiveTable where it will add all the multiples
 // of 5 utilizng a map
 const fiveTable = inputtable.map(multOfFive);
@@ -41,11 +42,9 @@ function valueOfSquares(value, index, array)
   return value * value;
 }
 
-// Question 3
-// Get the odd multiples of 5 between 1 and 100. 5, 15...
+// Question 3: Get the odd multiples of 5 between 1 and 100. 5, 15...
 
-// creating an array with 1-20 for the array to find the odd multiples
-// of 5
+// creating an array with 1-100
 const between1_100 = Array.from({ length: 100 }, (_, index) => index + 1);
 
 // variable that will hold the multiples of 5 utilizing filter
@@ -67,8 +66,7 @@ function oddFiveFunction(value, index, array)
   }
 }
 
-// Question 4
-// get the sum of even multiples of 7 between 1 and 100
+// Question 4: get the sum of even multiples of 7 between 1 and 100
 
 // variable that will hold the multiples of 7 utilizing filter
 const evenMultiplesSeven = between1_100.filter(evenMultiplesSevenFunction);
@@ -97,3 +95,58 @@ function sumOfValues(total, value, index, array)
 {
   return total + value; 
 }
+
+
+// Question 5: Use currying to rewrite the function:
+/*
+function cylinder_volume(r, h)
+{
+  var volume = 0.0;
+  volume = 3.14 * r * r * h;
+  return volume;
+}
+*/
+
+// Curried version of the function that calculates the function cylinder_volume
+function cylinder_volume(r) 
+{
+  return function (h) 
+  {
+    // original function variables
+    // initializing volume
+    var volume = 0.0;
+    // calculating volume
+    volume = 3.14 * r * r * h;
+    // returning the value of volume
+    return volume;
+  }
+}
+
+// Use r = 5 and h = 10 to call your curried function.
+var cylinder_volume_a = cylinder_volume(5)(10); 
+
+// Reuse the function from part ‘a’ but use h = 17
+var cylinder_volume_b = cylinder_volume(5)(17); 
+
+// Reuse the function from part ‘a’ but use h = 11
+var cylinder_volume_c = cylinder_volume(5)(11); 
+
+
+// DEBUG PRINT STATEMENTS
+console.log("\n2a) " + fiveTable); 
+
+console.log("\n2b) " + thirteenTable); 
+
+console.log("\n2c) " + squaresTable); 
+
+console.log("\n3) " + oddMultiplesFive);
+
+console.log("\n4a) " + evenMultiplesSeven);
+
+console.log("\n4b) " + sumOf7Even);
+
+console.log("\n5a) " + cylinder_volume_a);
+
+console.log("\n5b) " + cylinder_volume_b);
+
+console.log("\n5c) " + cylinder_volume_c);
